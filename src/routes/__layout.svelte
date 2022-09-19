c03cc9186cfe28f15aac8ab44e1b91e9a422d2f6
David Jablonski <web@davidjablonski.at>
2022-09-19 23:09:47 +0200

    add fathom

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,21 @@
     import Transition from '$lib/components/PageTransition.svelte';
     import Glossary from "$lib/components/Glossary.svelte";
 
+    import { onMount } from 'svelte'
+    import { browser } from '$app/env'
+    import { page } from '$app/stores'
+    import * as Fathom from 'fathom-client'
+
+    onMount(() => {
+      Fathom.load('RDBKIXJL', {
+        url: 'https://bloc-party-absolutely.klimadashboard.at/script.js',
+        includedDomains: ['klimadashboard.at', 'www.klimadashboard.at']
+      })
+    });
+
+    // track a page view when the pathname changes
+    $: $page.url.pathname, browser && Fathom.trackPageview()
+
     export let url;
 </script>
 
