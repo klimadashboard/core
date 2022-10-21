bee6827778dcd40da9833688c1802bdbe9873896
David Jablonski <web@davidjablonski.at>
2022-10-21 12:02:11 +0200

    open links in new tab in footer; add klimaforum

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,13 +1,13 @@
 <footer class="text-white bg-gray-800">
     <div class="container grid md:grid-cols-3 py-8">
       <nav class="flex space-x-4">
-        <a href="https://twitter.com/klimadashboard" aria-label="Twitter">
+        <a href="https://twitter.com/klimadashboard" aria-label="Twitter" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
          </svg>
         </a>
-        <a href="https://instagram.com/klimadashboard" aria-label="Instagram">
+        <a href="https://instagram.com/klimadashboard" aria-label="Instagram" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <rect x="4" y="4" width="16" height="16" rx="4"></rect>
@@ -15,13 +15,13 @@
             <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
          </svg>
         </a>
-        <a href="https://github.com/klimadashboard" aria-label="Instagram">
+        <a href="https://github.com/klimadashboard" aria-label="Instagram" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
          </svg>
         </a>
-        <a href="mailto:team@klimadashboard.at" aria-label="E-Mail">
+        <a href="mailto:team@klimadashboard.at" aria-label="E-Mail" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-at" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <circle cx="12" cy="12" r="4"></circle>
@@ -31,7 +31,9 @@
       </nav>
       <div></div>
       <nav class="flex">
-        <a href="https://klimadashboard.org" class="ml-auto">Unsere Organisation</a>
+        <a href="https://klimadashboard.org/forum" class="ml-auto" target="_blank">Klimaforum</a>
+        &bull;
+        <a href="https://klimadashboard.org" class="ml-auto" target="_blank">Über uns</a>
       </nav>
     </div>
     <div class="bg-gray-900 py-4">
@@ -42,7 +44,7 @@
                 <path opacity="0.2" d="M148.95 0H129.808C128.732 0 127.701 0.433664 126.948 1.20302L91.7358 37.203C90.2152 38.7577 90.2152 41.2423 91.7358 42.797L126.948 78.797C127.701 79.5663 128.732 80 129.808 80H148.95C152.498 80 154.29 75.7227 151.8 73.1939L121.887 42.8061C120.355 41.2492 120.355 38.7508 121.887 37.1939L151.8 6.80607C154.29 4.27726 152.498 0 148.95 0Z" fill="#F5F5F5"/>
             </svg>                
         <nav class="flex space-x-4">
-            <a href="https://klimadashboard.org/impressum">Impressum</a>
+            <a href="https://klimadashboard.org/impressum" target="_blank">Impressum</a>
         </nav>
         </div>
     </div>
