27e88e0ce9ad27079b17c78024488166112c9b79
David Jablonski <web@davidjablonski.at>
2022-11-05 14:56:37 +0100

    initial commit

---
+++ src/lib/components/charts/co2budgetPaths/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Papa from "papaparse";
+
+    $: rawData = [];
+
+    Papa.parse(
+    './data/scenarios_co2budget.csv',
+    {
+      download: true,
+      dynamicTyping: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        if (results) {
+          rawData = results.data;
+          console.log(rawData);
+        }
+      }
+    }
+    );
+</script>
+
+<div>
+
+</div>
