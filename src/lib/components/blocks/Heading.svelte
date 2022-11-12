76417817ae06accc2374f4fd82dcbe69d2be6222
David Jablonski <web@davidjablonski.at>
2022-11-12 18:01:08 +0100

    correct formatting of headings with HTML

--- src/lib/components/blocks/Heading.svelte
+++ src/lib/components/blocks/Heading.svelte
@@ -3,13 +3,11 @@
 </script>
 
 {#if block.content.level == "h1"}
-<h1 id="{block.id}" class="mt-20 text-4xl max-w-2xl tracking-tight mx-auto px-4">{block.content.text}</h1>
+<h1 id="{block.id}" class="mt-20 text-4xl max-w-2xl tracking-tight mx-auto px-4">{@html block.content.text}</h1>
 {:else if block.content.level == "h2"}
-<h2 id="{block.id}" class="mt-16 text-3xl max-w-2xl mx-auto px-4">{block.content.text}</h2>
+<h2 id="{block.id}" class="mt-16 text-3xl max-w-2xl mx-auto px-4">{@html block.content.text}</h2>
 {:else if block.content.level == "h3"}
-<h3 id="{block.id}" class="mt-8 text-xl max-w-2xl mx-auto px-4">{block.content.text}</h3>
+<h3 id="{block.id}" class="mt-8 text-xl max-w-2xl mx-auto px-4">{@html block.content.text}</h3>
 {:else if block.content.level == "h4"}
-<h4 id="{block.id}" class="mt-8 font-bold max-w-2xl mx-auto px-4">{block.content.text}</h4>
+<h4 id="{block.id}" class="mt-8 font-bold max-w-2xl mx-auto px-4">{@html block.content.text}</h4>
 {/if}
-
-
