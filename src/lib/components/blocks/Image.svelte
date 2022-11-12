2e99fa9b6a178503db0cb244c060233cde032f60
David Jablonski <web@davidjablonski.at>
2022-11-12 17:55:21 +0100

    fix image element

--- src/lib/components/blocks/Image.svelte
+++ src/lib/components/blocks/Image.svelte
@@ -1,7 +1,12 @@
 <script>
+    import { page } from "$app/stores";
+
     export let block;
+    console.log(block.content);
 </script>
 
+{#each block.content.image as image}
 <div class="my-4 px-4 text-lg {block.content.fullwidth == "true" ? "w-full" : "max-w-2xl mx-auto"}" id="{block.id}">
-{@html block.content.text}
+    <img src="https://klimadashboard.org/klimadashboard-at{$page.url.pathname}/{image}" alt="{block.content.alt}">
 </div>
+{/each}
