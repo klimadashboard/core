1ec2e8924abff01cb0c063ae4bcddab4e19bd61f
David Jablonski <web@davidjablonski.at>
2022-12-10 19:25:20 +0100

    simplify image import

--- src/lib/components/blocks/Image.svelte
+++ src/lib/components/blocks/Image.svelte
@@ -7,13 +7,13 @@
 {#if block.content.link}
 {#each block.content.image as image}
 <a class="my-4 px-4 text-lg {block.content.fullwidth == "true" ? "w-full" : "max-w-2xl mx-auto"}" id="{block.id}" href="{block.content.link}" target="_blank">
-    <img src="https://klimadashboard.org/klimadashboard-at{$page.url.pathname}/{image}" alt="{block.content.alt}">
+    <img src="https://klimadashboard.org/@/file/{image}" alt="{block.content.alt}">
 </a>
 {/each}
 {:else}
 {#each block.content.image as image}
 <div class="my-4 px-4 text-lg {block.content.fullwidth == "true" ? "w-full" : "max-w-2xl mx-auto"}" id="{block.id}">
-    <img src="https://klimadashboard.org/klimadashboard-at{$page.url.pathname}/{image}" alt="{block.content.alt}">
+    <img src="https://klimadashboard.org/@/file/{image}" alt="{block.content.alt}">
 </div>
 {/each}
 {/if}
