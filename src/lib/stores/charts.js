28c59a556a62d866a62e5daac72474e46e1ebde1
David Jablonski <web@davidjablonski.at>
2023-01-15 13:50:35 +0100

    change error handling in charts store

--- src/lib/stores/charts.js
+++ src/lib/stores/charts.js
@@ -1,16 +1,20 @@
 import { writable, derived } from 'svelte/store';
 import { error } from '@sveltejs/kit';
 
+
 const uri = "https://klimadashboard.org/de/charts.json";
 export const chartsData = writable([]);
 
-const fetchCharts = async () => {
+let fetchCharts = async () => {
 	const response = await fetch(uri)
     .then((x) => x.json())
     .catch(function(err){
-        throw error(500, 'Timeout when loading chart. ' + err);
+        console.log('Timeout when loading chart. ' + err);
     });
+
+    if(response) {
     chartsData.set(response);
+    }
 }
 
 fetchCharts();
