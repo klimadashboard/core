b9a1ab91279f5fabc455c11b69e046c8c5e93b13
David Jablonski <web@davidjablonski.at>
2023-01-18 20:21:49 +0100

    improve error handling for page load

--- src/routes/[...slug]/+page.server.js
+++ src/routes/[...slug]/+page.server.js
@@ -10,12 +10,12 @@ locale.subscribe(value => {
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
   const url = "https://cms.klimadashboard.org/" + localeString + "/klimadashboard-" + "at" + (params.slug !== "" ? "/" : "") + params.slug + ".json";
-  const res = await fetch(url);
-  const item = await res.json();
- 
-  if (item) {
-    return item;
-  }
- 
-  throw error(404, 'Not found');
+  
+  const promise = await fetch(url)
+  .then((x) => x.json())
+  .catch(function(err){
+    throw error(404, 'Timeout when loading page data. ' + err);
+  });
+  
+  return promise;
 }
