af01fe01f3d07bf2eed0c3856ac251661769beb3
David Jablonski <web@davidjablonski.at>
2023-04-17 14:20:25 +0200

    first version of international consumption-based chart

--- src/lib/components/charts/consumptionBasedEmissions/index.svelte
+++ src/lib/components/charts/consumptionBasedEmissions/index.svelte
@@ -2,11 +2,14 @@
 	import BarChart from '../chartBar.svelte';
 	import Papa from 'papaparse';
 	import Loader from '$lib/components/Loader.svelte';
+	import { PUBLIC_VERSION } from '$env/static/public';
 
-	$: rawDataProduction = [];
+	let rawDataProduction;
+	let rawDataConsumption;
+	let region;
 
 	Papa.parse(
-		'https://data.klimadashboard.org/at/emissions/Emission_All_sectors_Austria_1990-2020.csv',
+		'https://data.klimadashboard.org/' + PUBLIC_VERSION + '/emissions/emissions_by_sectors.csv',
 		{
 			download: true,
 			dynamicTyping: true,
@@ -20,61 +23,55 @@
 		}
 	);
 
-	const rawDataConsumption = [
+	Papa.parse(
+		'https://data.klimadashboard.org/' +
+			PUBLIC_VERSION +
+			'/emissions/emissions_consumption_based.csv',
 		{
-			year: 1997,
-			value: 116868000
-		},
-		{
-			year: 2001,
-			value: 120069000
-		},
-		{
-			year: 2004,
-			value: 130950000
-		},
-		{
-			year: 2007,
-			value: 123238000
-		},
-		{
-			year: 2011,
-			value: 123636000
-		}
-	];
-
-	$: dataset = rawDataProduction?.map((entry, i) => {
-		return {
-			label: entry.year,
-			annotation: entry.year == 2001 ? '' : '',
-			categories: [
-				{
-					label: 'Konsumbasierte Emissionen',
-					value: rawDataConsumption.filter((d) => d.year == entry.year)[0]
-						? rawDataConsumption.filter((d) => d.year == entry.year)[0].value
-						: 0,
-					color: '#E59E1A'
-				},
-				{
-					label: 'Produktionsbasierte Emissionen',
-					value: entry.total_co2e_t,
-					color: '#4DB263'
+			download: true,
+			dynamicTyping: true,
+			header: true,
+			skipEmptyLines: true,
+			complete: function (results) {
+				if (results) {
+					rawDataConsumption = results.data;
 				}
-			]
-		};
-	});
+			}
+		}
+	);
+
+	$: if (PUBLIC_VERSION == 'at') {
+		region = 'Austria';
+	} else {
+		region = 'Germany';
+	}
+
+	$: dataset = rawDataProduction
+		?.filter((d) => d.region == region)
+		.map((entry, i) => {
+			return {
+				label: entry.year,
+				categories: [
+					{
+						label: 'Konsumbasierte Emissionen',
+						value: rawDataConsumption?.filter((d) => d.year == entry.year)[0]
+							? rawDataConsumption.filter((d) => d.year == entry.year)[0].value
+							: 0,
+						color: '#E59E1A'
+					},
+					{
+						label: 'Produktionsbasierte Emissionen',
+						value: entry.total_co2e_t,
+						color: '#4DB263'
+					}
+				]
+			};
+		});
 </script>
 
 <div class="h-80">
 	{#if dataset}
-		<BarChart
-			data={dataset}
-			visualisation="grouped"
-			xAxixInterval="5"
-			label={'Konsumbasierte Emissionen 1990 - 2015'}
-			unit={'t THG'}
-			showDifferenceArea={true}
-		/>
+		<BarChart data={dataset} visualisation="grouped" xAxixInterval="5" unit={'t THG'} />
 	{:else}
 		<Loader />
 	{/if}
