826b0e8c7bba262618d9628b61277209bd4645f3
David Jablonski <web@davidjablonski.at>
2023-04-24 11:19:03 +0200

    update path for federalStates

--- src/lib/components/charts/emissionsByFederalStates/index.svelte
+++ src/lib/components/charts/emissionsByFederalStates/index.svelte
@@ -1,19 +1,20 @@
 <script>
 	import Papa from 'papaparse';
 	import Chart from './Chart.svelte';
+	import { PUBLIC_VERSION } from '$env/static/public';
 
 	$: dataset = null;
 	$: readMore = false;
 
 	Papa.parse(
-		'https://data.klimadashboard.org/at/emissions/Emission_All_sectors_Bundeslaender_Austria_1990-2019.csv',
+		'https://data.klimadashboard.org/' + PUBLIC_VERSION + '/emissions/emissions_by_sectors.csv',
 		{
 			download: true,
 			dynamicTyping: true,
 			header: true,
 			complete: function (results) {
 				if (results) {
-					dataset = results.data;
+					dataset = results.data.filter((d) => d.region !== 'Austria');
 				}
 			}
 		}
