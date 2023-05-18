b7d2e078f3f35cadd5d30952bef152304567d2fd
David Jablonski <web@davidjablonski.at>
2023-05-18 20:34:44 +0200

    initial commit

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,70 +1,76 @@
 <script>
-import { page } from '$app/stores';
+	import { page } from '$app/stores';
 
-export let data;
+	export let data;
 
-const shareData = {
-	title: data.heading,
-	text: data.intro,
-	url: $page.url
-};
-
+	const shareData = {
+		title: data.heading,
+		text: data.intro,
+		url: $page.url
+	};
 </script>
 
-<section
-	id="page-header"
-	class="bg-gradient-green shadow-inner text-white pt-16 pb-8 mb-8 relative"
-	style={data.cover_styles}
->
+<section id="page-header" class="text-white pt-32" style={data.cover_styles}>
 	{#if data.cover_background}
-		<img src="https://klimadashboard.org/@/file/{data.cover_background.replace("- file://","")}"
-		class="object-cover absolute inset-0 w-full h-full"
-		alt="" />
+		<img
+			src="https://klimadashboard.org/@/file/{data.cover_background.replace('- file://', '')}"
+			class="object-cover absolute inset-0 w-full h-full"
+			alt=""
+		/>
 	{/if}
 	<div class="container z-10 relative">
-		<div class="max-w-2xl mx-auto break-words">
-			<div class="flex gap-2 datas-center">
-				<span class="uppercase font-semibold tracking-wider">{data.eyebrow}</span>
-				<button
-					id="share"
-					on:mousedown={() => navigator.share(shareData)}
-					aria-label="Seite teilen"
+		<div class="flex gap-2 datas-center">
+			<span class="uppercase font-semibold tracking-wider">{data.eyebrow}</span>
+			<button id="share" on:mousedown={() => navigator.share(shareData)} aria-label="Seite teilen">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="icon icon-tabler icon-tabler-share"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					stroke-width="2"
+					stroke="currentColor"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="icon icon-tabler icon-tabler-share"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						stroke-width="2"
-						stroke="currentColor"
-						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-						<circle cx="6" cy="12" r="3" />
-						<circle cx="18" cy="6" r="3" />
-						<circle cx="18" cy="18" r="3" />
-						<line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
-						<line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
-					</svg>
-				</button>
-			</div>
-			<h1 class="text-4xl sm:text-5xl tracking-tight my-2">{data.heading}</h1>
-			<div class="text-xl mt-4 page-intro">
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<circle cx="6" cy="12" r="3" />
+					<circle cx="18" cy="6" r="3" />
+					<circle cx="18" cy="18" r="3" />
+					<line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
+					<line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
+				</svg>
+			</button>
+		</div>
+		<h1 class="text-5xl sm:text-6xl tracking-tight my-2">Die Klimakrise in Österreich</h1>
+		<div class="text-xl mt-4 page-intro">
 			{@html data.intro}
-			</div>
+		</div>
+		<p class="text-xl">
+			Datensätze und Grafiken zu Herausforderungen, Auswirkungen und Lösungen der Klimakrise.
+			Wissenschaftlich, unabhängig, zugänglich für alle.
+		</p>
+
+		<div class="flex space-x-2 mt-8">
+			<a href="#" class="bg-white p-2 text-black">
+				<h3>Treibhausgasbudget</h3>
+				<p>Die 1,5-Grad Grenze wird gesprengt.</p>
+			</a>
 		</div>
 	</div>
 </section>
 
 <style>
 	:global(.page-intro ul) {
-        @apply list-disc;
-    }
+		@apply list-disc;
+	}
 
-    :global(.page-intro p, .page-intro ul) {
-        @apply my-1;
-    }
+	:global(.page-intro p, .page-intro ul) {
+		@apply my-1;
+	}
+
+	#page-header {
+		background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+	}
 </style>
--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -12,52 +12,82 @@
 		});
 
 	$: showNav = false;
+
+	const structure = [
+		{
+			label: 'Regionen',
+			children: [
+				{
+					label: 'Vor Ort',
+					comingSoon: true
+				},
+				{
+					label: 'Niederösterreich'
+				},
+				{
+					label: 'Wien'
+				},
+				{
+					label: 'Salzburg'
+				},
+				{
+					label: 'Oberösterreich'
+				}
+			]
+		},
+		{
+			label: 'Sektoren',
+			children: [
+				{
+					label: 'Energie'
+				},
+				{
+					label: 'Mobilität'
+				},
+				{
+					label: 'Landwirtschaft',
+					comingSoon: true
+				},
+				{
+					label: 'Industrie',
+					comingSoon: true
+				},
+				{
+					label: 'Gebäude',
+					comingSoon: true
+				}
+			]
+		},
+		{
+			label: 'Auswirkungen',
+			children: [
+				{
+					label: 'Temperatur'
+				},
+				{
+					label: 'Gletscher'
+				}
+			]
+		},
+		{
+			label: 'Themen',
+			children: [
+				{
+					label: 'Treibhausgasbudget'
+				},
+				{
+					label: 'Emissionen'
+				}
+			]
+		}
+	];
 </script>
 
-<header class="fixed py-3 w-full z-50 bg-white ">
-	<div class="container flex justify-between items-center">
-		<a href="/" aria-label="Startseite" on:mouseup={() => (showNav = false)}>
-			<svg
-				width="256"
-				height="256"
-				viewBox="0 0 256 256"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-10 w-10 rounded"
-			>
-				<rect width="256" height="256" fill="url(#kd-gradient-header)" />
-				<path
-					d="M119.45 88H53C50.7909 88 49 89.7909 49 92V164C49 166.209 50.7909 168 53 168H119.45C122.998 168 124.79 163.723 122.3 161.194L92.3872 130.806C90.8547 129.249 90.8547 126.751 92.3872 125.194L122.3 94.8061C124.79 92.2773 122.998 88 119.45 88Z"
-					fill="#DBF0E0"
-				/>
-				<path
-					opacity="0.6"
-					d="M162.95 88H134.808C133.732 88 132.701 88.4337 131.948 89.203L96.7358 125.203C95.2152 126.758 95.2152 129.242 96.7358 130.797L131.948 166.797C132.701 167.566 133.732 168 134.808 168H162.95C166.498 168 168.29 163.723 165.8 161.194L135.887 130.806C134.355 129.249 134.355 126.751 135.887 125.194L165.8 94.8061C168.29 92.2773 166.498 88 162.95 88Z"
-					fill="#DBF0E0"
-				/>
-				<path
-					opacity="0.2"
-					d="M197.95 88H178.808C177.732 88 176.701 88.4337 175.948 89.203L140.736 125.203C139.215 126.758 139.215 129.242 140.736 130.797L175.948 166.797C176.701 167.566 177.732 168 178.808 168H197.95C201.498 168 203.29 163.723 200.8 161.194L170.887 130.806C169.355 129.249 169.355 126.751 170.887 125.194L200.8 94.8061C203.29 92.2773 201.498 88 197.95 88Z"
-					fill="#DBF0E0"
-				/>
-				<defs>
-					<linearGradient
-						id="kd-gradient-header"
-						x1="425"
-						y1="8.00003"
-						x2="16"
-						y2="248"
-						gradientUnits="userSpaceOnUse"
-					>
-						<stop stop-color="#A3D58A" />
-						<stop offset="1" stop-color="#28A889" />
-					</linearGradient>
-				</defs>
-			</svg>
-		</a>
-
-		<nav class="flex gap-4">
-			<button on:mousedown={() => (showNav = !showNav)} class="sm:hidden" aria-label="Navigation">
+<header class="fixed w-full z-10">
+	<div class="bg-white">
+		<div class="container flex gap-8 items-center text-xl">
+			<a href="/" class="bg-gray-600 w-16 h-16 block" />
+			<button class="md:hidden flex">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="icon icon-tabler icon-tabler-menu-2"
@@ -71,79 +101,85 @@
 					stroke-linejoin="round"
 				>
 					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<line x1="4" y1="6" x2="20" y2="6" />
-					<line x1="4" y1="12" x2="20" y2="12" />
-					<line x1="4" y1="18" x2="20" y2="18" />
+					<path d="M4 6l16 0" />
+					<path d="M4 12l16 0" />
+					<path d="M4 18l16 0" />
 				</svg>
+				<span>Menü</span>
 			</button>
-			{#await promise then children}
-				<ul
-					class="bg-white gap-2 sm:gap-4 {showNav
-						? 'flex flex-col absolute inset-0 h-screen top-14 p-4'
-						: 'hidden'} sm:flex"
-				>
-					{#each children.filter((d) => d.num > 0 && d.parent == 'klimadashboard-' + PUBLIC_VERSION) as child}
-						<li class="group relative">
-							<a
-								href={child.uri.replace('klimadashboard-' + PUBLIC_VERSION, '')}
-								class="navigation-link"
-								on:mouseup={() => (showNav = false)}>{child.content.title}</a
-							>
-							{#if children.filter((c) => child.id == c.parent && c !== child)}
-								<ul
-									class="sm:opacity-0 sm:pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 transition sm:absolute top-full -left-2 p-2 bg-white w-64"
-								>
-									{#each children.filter((c) => child.id == c.parent && c !== child) as grandchild}
-										<li>
-											<a
-												href={grandchild.id.replace('klimadashboard-' + PUBLIC_VERSION + '/', '/')}
-												class="navigation-link font-bold"
-												on:mouseup={() => (showNav = false)}>{grandchild.content.title}</a
-											>
-											<ul>
-												{#each children.filter((c) => c.id.includes(grandchild.uri) && c !== grandchild) as greatgrandchild}
-													<li>
-														<a
-															href={greatgrandchild.id.replace(
-																'klimadashboard-' + PUBLIC_VERSION + '/',
-																'/'
-															)}
-															on:mouseup={() => (showNav = false)}
-															class="navigation-link">{greatgrandchild.content.title}</a
-														>
-													</li>
-												{/each}
-											</ul>
-										</li>
+			<nav
+				class="absolute top-8 h-screen left-0 right-0 bg-white md:relative md:block md:w-auto md:h-auto z-50"
+			>
+				<ul class="flex flex-col md:flex-row space-x-4">
+					{#each structure as item}
+						<li>
+							<h3>{item.label}</h3>
+							{#if item.children}
+								<ul class="absolute top-8 flex gap-4">
+									{#each item.children as child}
+										<li>{child.label}</li>
 									{/each}
 								</ul>
 							{/if}
 						</li>
 					{/each}
 				</ul>
-			{:catch error}
-				{error}
-			{/await}
-
-			<!--
-            <button class="flex items-center" on:mousedown={() => changeLocale()} aria-label="Language Switch">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-language" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M4 5h7"></path>
-                <path d="M9 3v2c0 4.418 -2.239 8 -5 8"></path>
-                <path d="M5 9c-.003 2.144 2.952 3.908 6.7 4"></path>
-                <path d="M12 20l4 -9l4 9"></path>
-                <path d="M19.1 18h-6.2"></path>
-             </svg>
-             <span class="uppercase font-semibold">{["de","en"].find(d => d !== $locale)}</span>
-            </button>
-            -->
-		</nav>
+			</nav>
+			<nav class="ml-auto">
+				<ul class="flex gap-2 text-sm font-bold uppercase tracking-wide">
+					<li class="flex flex-col items-center opacity-70 hover:opacity-100">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="icon icon-tabler icon-tabler-info-circle"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+							<path d="M12 9h.01" />
+							<path d="M11 12h1v4h1" />
+						</svg>
+						<span>Info</span>
+					</li>
+					<li class="flex flex-col items-center opacity-70 hover:opacity-100">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="icon icon-tabler icon-tabler-chart-area-line"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							stroke-width="2"
+							stroke="currentColor"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+							<path d="M4 19l4 -6l4 2l4 -5l4 4l0 5l-16 0" />
+							<path d="M4 12l3 -4l4 2l5 -6l4 4" />
+						</svg>
+						<span>Charts</span>
+					</li>
+					<li>Glossar</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<div class="bg-gray-100 py-4">
+		<div class="container flex gap-8">
+			<nav>
+				<ul class="flex text-lg gap-4">
+					<li>Gesellschaft</li>
+					<li>Unternehmen</li>
+					<li>Emissionen</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
 </header>
-
-<style>
-	.navigation-link:hover {
-		@apply border-b;
-	}
-</style>
