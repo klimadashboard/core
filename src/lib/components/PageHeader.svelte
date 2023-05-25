fc973a43098c13bed05220a847c449ed1d4cae9a
David Jablonski <web@davidjablonski.at>
2023-05-25 10:52:02 +0200

    new navigation

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -10,7 +10,7 @@
 	};
 </script>
 
-<section id="page-header" class="text-white pt-32" style={data.cover_styles}>
+<section id="page-header" class="pt-20 pb-4" style={data.cover_styles}>
 	{#if data.cover_background}
 		<img
 			src="https://klimadashboard.org/@/file/{data.cover_background.replace('- file://', '')}"
@@ -19,44 +19,39 @@
 		/>
 	{/if}
 	<div class="container z-10 relative">
-		<div class="flex gap-2 datas-center">
-			<span class="uppercase font-semibold tracking-wider">{data.eyebrow}</span>
-			<button id="share" on:mousedown={() => navigator.share(shareData)} aria-label="Seite teilen">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="icon icon-tabler icon-tabler-share"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					stroke-width="2"
-					stroke="currentColor"
-					fill="none"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+		<div class="max-w-2xl">
+			<div class="flex gap-2">
+				<span class="uppercase font-semibold tracking-wider">{data.eyebrow}</span>
+				<button
+					id="share"
+					on:mousedown={() => navigator.share(shareData)}
+					aria-label="Seite teilen"
 				>
-					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-					<circle cx="6" cy="12" r="3" />
-					<circle cx="18" cy="6" r="3" />
-					<circle cx="18" cy="18" r="3" />
-					<line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
-					<line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
-				</svg>
-			</button>
-		</div>
-		<h1 class="text-5xl sm:text-6xl tracking-tight my-2">Die Klimakrise in Österreich</h1>
-		<div class="text-xl mt-4 page-intro">
-			{@html data.intro}
-		</div>
-		<p class="text-xl">
-			Datensätze und Grafiken zu Herausforderungen, Auswirkungen und Lösungen der Klimakrise.
-			Wissenschaftlich, unabhängig, zugänglich für alle.
-		</p>
-
-		<div class="flex space-x-2 mt-8">
-			<a href="#" class="bg-white p-2 text-black">
-				<h3>Treibhausgasbudget</h3>
-				<p>Die 1,5-Grad Grenze wird gesprengt.</p>
-			</a>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-share"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<circle cx="6" cy="12" r="3" />
+						<circle cx="18" cy="6" r="3" />
+						<circle cx="18" cy="18" r="3" />
+						<line x1="8.7" y1="10.7" x2="15.3" y2="7.3" />
+						<line x1="8.7" y1="13.3" x2="15.3" y2="16.7" />
+					</svg>
+				</button>
+			</div>
+			<h1 class="text-3xl sm:text-5xl tracking-tight my-2">{data.heading}</h1>
+			<div class="text-xl mt-4 page-intro">
+				{@html data.intro}
+			</div>
 		</div>
 	</div>
 </section>
@@ -70,7 +65,9 @@
 		@apply my-1;
 	}
 
+	/*
 	#page-header {
 		background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
 	}
+	*/
 </style>
--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -15,26 +15,17 @@
 
 	const structure = [
 		{
-			label: 'Regionen',
+			label: 'Emissionen',
 			children: [
 				{
-					label: 'Vor Ort',
-					comingSoon: true
+					label: 'Vergangenheit'
 				},
 				{
-					label: 'Niederösterreich'
-				},
-				{
-					label: 'Wien'
-				},
-				{
-					label: 'Salzburg'
-				},
-				{
-					label: 'Oberösterreich'
+					label: 'Treibhausgasbudget'
 				}
 			]
 		},
+
 		{
 			label: 'Sektoren',
 			children: [
@@ -59,6 +50,27 @@
 			]
 		},
 		{
+			label: 'Regionen',
+			children: [
+				{
+					label: 'Vor Ort',
+					comingSoon: true
+				},
+				{
+					label: 'Niederösterreich'
+				},
+				{
+					label: 'Wien'
+				},
+				{
+					label: 'Salzburg'
+				},
+				{
+					label: 'Oberösterreich'
+				}
+			]
+		},
+		{
 			label: 'Auswirkungen',
 			children: [
 				{
@@ -70,23 +82,53 @@
 			]
 		},
 		{
-			label: 'Themen',
-			children: [
-				{
-					label: 'Treibhausgasbudget'
-				},
-				{
-					label: 'Emissionen'
-				}
-			]
+			label: 'Gesellschaft'
 		}
 	];
 </script>
 
-<header class="fixed w-full z-10">
+<header class="fixed w-full z-50 border-b-2 border-b-green-600">
 	<div class="bg-white">
 		<div class="container flex gap-8 items-center text-xl">
-			<a href="/" class="bg-gray-600 w-16 h-16 block" />
+			<a href="/" class="">
+				<svg
+					width="256"
+					height="256"
+					viewBox="0 0 256 256"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-12 w-12 rounded"
+				>
+					<rect width="256" height="256" fill="url(#kd-gradient-header)" />
+					<path
+						d="M119.45 88H53C50.7909 88 49 89.7909 49 92V164C49 166.209 50.7909 168 53 168H119.45C122.998 168 124.79 163.723 122.3 161.194L92.3872 130.806C90.8547 129.249 90.8547 126.751 92.3872 125.194L122.3 94.8061C124.79 92.2773 122.998 88 119.45 88Z"
+						fill="#DBF0E0"
+					/>
+					<path
+						opacity="0.6"
+						d="M162.95 88H134.808C133.732 88 132.701 88.4337 131.948 89.203L96.7358 125.203C95.2152 126.758 95.2152 129.242 96.7358 130.797L131.948 166.797C132.701 167.566 133.732 168 134.808 168H162.95C166.498 168 168.29 163.723 165.8 161.194L135.887 130.806C134.355 129.249 134.355 126.751 135.887 125.194L165.8 94.8061C168.29 92.2773 166.498 88 162.95 88Z"
+						fill="#DBF0E0"
+					/>
+					<path
+						opacity="0.2"
+						d="M197.95 88H178.808C177.732 88 176.701 88.4337 175.948 89.203L140.736 125.203C139.215 126.758 139.215 129.242 140.736 130.797L175.948 166.797C176.701 167.566 177.732 168 178.808 168H197.95C201.498 168 203.29 163.723 200.8 161.194L170.887 130.806C169.355 129.249 169.355 126.751 170.887 125.194L200.8 94.8061C203.29 92.2773 201.498 88 197.95 88Z"
+						fill="#DBF0E0"
+					/>
+					<defs>
+						<linearGradient
+							id="kd-gradient-header"
+							x1="425"
+							y1="8.00003"
+							x2="16"
+							y2="248"
+							gradientUnits="userSpaceOnUse"
+						>
+							<stop stop-color="#A3D58A" />
+							<stop offset="1" stop-color="#28A889" />
+						</linearGradient>
+					</defs>
+				</svg>
+			</a>
 			<button class="md:hidden flex">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -107,19 +149,19 @@
 				</svg>
 				<span>Menü</span>
 			</button>
-			<nav
-				class="absolute top-8 h-screen left-0 right-0 bg-white md:relative md:block md:w-auto md:h-auto z-50"
-			>
-				<ul class="flex flex-col md:flex-row space-x-4">
+			<nav class="hidden md:flex items-center">
+				<ul class="flex flex-col md:flex-row space-x-6">
 					{#each structure as item}
-						<li>
-							<h3>{item.label}</h3>
+						<li class="group">
+							<a class="leading-[4rem]" href="#">{item.label}</a>
 							{#if item.children}
-								<ul class="absolute top-8 flex gap-4">
-									{#each item.children as child}
-										<li>{child.label}</li>
-									{/each}
-								</ul>
+								<div class="absolute left-0 top-16 md:top-16 bg-gradient-green w-screen text-white">
+									<ul class="md:container flex gap-4 py-4 md:hidden group-hover:flex">
+										{#each item.children as child}
+											<li>{child.label}</li>
+										{/each}
+									</ul>
+								</div>
 							{/if}
 						</li>
 					{/each}
@@ -167,17 +209,6 @@
 						<span>Charts</span>
 					</li>
 					<li>Glossar</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-	<div class="bg-gray-100 py-4">
-		<div class="container flex gap-8">
-			<nav>
-				<ul class="flex text-lg gap-4">
-					<li>Gesellschaft</li>
-					<li>Unternehmen</li>
-					<li>Emissionen</li>
 				</ul>
 			</nav>
 		</div>
