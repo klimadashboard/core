e0bdbe1c8877c5bd5391a373ff4f180ec6047704
David Jablonski <web@davidjablonski.at>
2023-05-26 17:49:41 +0200

    add redirect feature

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -1,12 +1,19 @@
 <script>
+	import { onMount } from 'svelte';
 	import Blocks from '$lib/components/blocks/index.svelte';
 	import PageHeader from '$lib/components/PageHeader.svelte';
 	import SectionHeader from '$lib/components/SectionHeader.svelte';
-	import { page } from '$app/stores';
+	import Loader from '$lib/components/Loader.svelte';
 	import { fade } from 'svelte/transition';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
 
 	/** @type {import('./$types').PageData} */
 	export let data = [];
+
+	$: if (browser && data.redirecturl) {
+		goto(data.redirecturl);
+	}
 </script>
 
 <svelte:head>
@@ -14,23 +21,27 @@
 	<meta name="description" content={data.meta_description} />
 </svelte:head>
 
-<main class="mb-24">
-	<PageHeader {data} />
-
-	{#key data}
-		{#if data.pagelayout}
-			{#each JSON.parse(data.pagelayout) as layout}
-				<section id="{layout.id} {layout.attrs.id}" class={layout.attrs.class} transition:fade>
-					{#if layout.attrs.headline}
-						<SectionHeader attrs={layout.attrs} />
-					{/if}
-					{#each layout.columns as column}
-						<Blocks content={column.blocks} />
-					{/each}
-				</section>
-			{/each}
-		{:else}
-			Content not found.
-		{/if}
-	{/key}
-</main>
+{#key data}
+	{#if data.redirecturl}
+		<Loader />
+	{:else}
+		<main class="mb-24">
+			<PageHeader {data} />
+			{#if data.pagelayout}
+				{#each JSON.parse(data.pagelayout) as layout}
+					<section id="{layout.id} {layout.attrs.id}" class={layout.attrs.class} transition:fade>
+						{#if layout.attrs.headline}
+							<SectionHeader attrs={layout.attrs} />
+						{/if}
+						{#each layout.columns as column}
+							<Blocks content={column.blocks} />
+						{/each}
+					</section>
+				{/each}
+			{:else}
+				<Loader />
+				Content not found.
+			{/if}
+		</main>
+	{/if}
+{/key}
