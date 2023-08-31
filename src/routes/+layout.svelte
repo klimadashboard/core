304e803b5dcaee6aa75d1693673e27dc85a4bfdc
David Jablonski <web@davidjablonski.at>
2023-08-31 17:29:16 +0200

    update fathom analytics code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
 <script>
 	import '../app.css';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
 	import { glossaryItem } from '$lib/stores/glossary';
 	import Glossary from '$lib/components/Glossary.svelte';
+	import { onMount } from 'svelte';
+	import * as Fathom from 'fathom-client';
+	import { PUBLIC_VERSION } from '$env/static/public';
+
+	const fathom_ids = [
+		{
+			version: 'de',
+			fathom: 'BKRABNNN'
+		},
+		{
+			version: 'at',
+			fathom: 'RDBKIXJL'
+		}
+	];
+
+	onMount(() => {
+		Fathom.load(fathom_ids.find((d) => d.version == PUBLIC_VERSION).fathom, {
+			url: 'https://cdn-eu.usefathom.com/script.js'
+		});
+	});
+
+	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 
 {#if $glossaryItem}
