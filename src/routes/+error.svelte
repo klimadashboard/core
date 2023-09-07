5873564a1d2f5aff8954a7a3d6beee088cdd71fa
David Jablonski <web@davidjablonski.at>
2023-09-07 23:02:52 +0200

    improved error message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,27 @@
 <script>
+	import Header from '$lib/components/header.svelte';
+	import Footer from '$lib/components/footer.svelte';
 	import { page } from '$app/stores';
 </script>
 
-<div class="container py-16">
-	<h1 class="text-4xl mb-4">Entschuldigung, hier gab es einen Fehler.</h1>
-	<p>{$page.error.message}</p>
-</div>
+<Header />
+
+<main
+	class="pt-32 container"
+	itemscope
+	itemtype="https://schema.org/FAQPage"
+	style="min-height:60vh;"
+>
+	<h1 class="text-4xl mb-4">Entschuldigung! 🚨</h1>
+	<p class="text-lg max-w-2xl">
+		Diese Seite konnte nicht gefunden werden oder ein Teil dieser Seite ist fehlerhaft. Navigieren
+		Sie zu einer anderen Seite oder probieren Sie es später erneut. Falls dieser Fehler bestehen
+		bleibt, schreiben Sie uns gern an <a
+			href="mailto:team@klimadashboard.org"
+			class="underline underline-offset-2">team@klimadashboard.org</a
+		>.
+	</p>
+	<p class="font-mono mt-16">{$page.error.message}</p>
+</main>
+
+<Footer />
