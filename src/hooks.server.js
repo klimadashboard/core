46ba0d104b8cb5ea9abe892f526b23697c1bc6de
David Jablonski <web@davidjablonski.at>
2024-01-17 11:25:08 +0100

    Update hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Handle} */ export function handle({ event, resolve }) {
 	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace('%lang%', 'en')
+		transformPageChunk: ({ html }) => html.replace('%lang%', 'de')
 	});
 }
