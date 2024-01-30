6ab96886eacb13b7d63becebdb62d4a0308089bd
David Jablonski <web@davidjablonski.at>
2024-01-30 21:20:54 +0100

    fix x-axis

--- src/lib/components/charts/globalEmissions/Chart.svelte
+++ src/lib/components/charts/globalEmissions/Chart.svelte
@@ -13,7 +13,7 @@
 
 	$: chartHeight = (data.length + 1) * yHeight + margin.top + margin.bottom;
 
-	$: maxValue = Math.max(max(data, (d) => d.value) * 1.2, 20);
+	$: maxValue = Math.max(max(data, (d) => d.value) * 1.25, 0);
 
 	$: xScale = scaleLinear()
 		.range([0, chartWidth - margin.left - margin.right])
@@ -28,15 +28,22 @@
 <div class="" style="height: {chartHeight}px" bind:clientWidth={chartWidth}>
 	{#if chartWidth && chartHeight}
 		<svg width="100%" height="100%">
-			{#each xScale.ticks() as tick}
+			{#each xScale.ticks() as tick, i}
 				<g transform={`translate(${margin.left + xScale(tick)}, 4)`} class="text-xs text-gray-500">
-					<text fill="currentColor" text-anchor="middle" dominant-baseline="hanging">{tick}t</text>
+					<text fill="currentColor" text-anchor="middle" dominant-baseline="hanging"
+						>{tick}
+						{i + 1 == xScale.ticks().length ? 't THG' : ''}</text
+					>
 					<line x1={0} x2={0} y1={14} y2={chartHeight} class="stroke-gray-200 stroke-1" />
 				</g>
 			{/each}
 			{#if worldAverage > 0}
 				<g transform="translate({xScale(worldAverage) + margin.left},4)">
+					<rect width={62} height={10} class="fill-white" />
 					<text dominant-baseline="hanging" text-anchor="middle" class="text-sm font-bold">🌍</text>
+					<text class="text-xs text-gray-800 font-bold" dominant-baseline="hanging" x={10}
+						>{worldAverage}t THG</text
+					>
 					<line x1={0} x2={0} y1={10} y2={chartHeight} class="stroke-gray-600 stroke-1" />
 				</g>
 			{/if}
