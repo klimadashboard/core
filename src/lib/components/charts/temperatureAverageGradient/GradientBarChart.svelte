817f85198ed08726d7175300b54baa003e141fc0
Markus Doppler <48652934+markusdoppler@users.noreply.github.com>
2024-02-16 12:52:30 +0100

    basic bar chart

--- src/lib/components/charts/temperatureAverageGradient/GradientBarChart.svelte
+++ src/lib/components/charts/temperatureAverageGradient/GradientBarChart.svelte
@@ -1,24 +1,10 @@
-<style>
-	.hsl-colors {
-		color: hsl(216, 86%, 23%);
-		color: hsl(208, 69%, 42%);
-		color: hsl(204, 54%, 52%);
-		color: hsl(202, 57%, 63%);
-		color: hsl(201, 53%, 75%);
-		color: hsl(209, 56%, 86%);
-		color: hsl(209, 61%, 92%);
-		color: hsl(19, 96%, 91%);
-		color: hsl(17, 94%, 81%);
-		color: hsl(14, 96%, 72%);
-		color: hsl(11, 96%, 64%);
-		color: hsl(5, 86%, 55%);
-	}
-</style>
-
 <script>
 	import { max, min, extent, bisector } from 'd3-array';
 	import { scaleLinear, scaleOrdinal } from 'd3-scale';
 	import { fade, draw } from 'svelte/transition';
+	import { selectedStation } from '$lib/stores/weather';
+	import Papa from 'papaparse';
+	import { PUBLIC_VERSION } from '$env/static/public';
 
 	export let data;
 	export let averageTemperature;
@@ -30,12 +16,12 @@
 	let chartHeight;
 	let chartWidth;
 
-	const margin = { top: 10, right: 50, bottom: 10, left: 0 };
+	const margin = { top: 10, right: 50, bottom: 10, left: 150 };
 	$: innerChartHeight = chartHeight - margin.top - margin.bottom;
 	$: innerChartWidth = chartWidth - margin.left - margin.right;
 
 	// data
-	$: console.log("averageTemperature", averageTemperature)
+	$: console.log('averageTemperature', averageTemperature);
 
 	// drawing preparation
 	const colors = [
@@ -44,13 +30,13 @@
 		'#4292c6',
 		'#6baed6',
 		'#9ecae1',
-		'#c6dbef',
+		// '#c6dbef',
 		'#deebf7',
 		'#fee0d2',
 		'#fcbba1',
 		'#fc9272',
 		'#fb6a4a',
-		'#ef3b2c',
+		'#ef3b2c'
 	];
 
 	$: xScale = scaleLinear()
@@ -61,20 +47,53 @@
 		.rangeRound([innerChartHeight, 0])
 		.domain([min(data, (d) => d.averageTemperature), max(data, (d) => d.averageTemperature)]);
 
+	// $: colorScale = scaleLinear()
+	// 	.range(colors)
+	// 	.domain([min(data, (d) => d.averageTemperature), max(data, (d) => d.averageTemperature)]);
+
 	$: colorScale = scaleLinear()
 		.range(colors)
-		.domain([min(data, (d) => d.averageTemperature), max(data, (d) => d.averageTemperature)]);
+		.domain(
+			colors.map((_, i) => {
+				return averageTemperature + (i - Math.ceil(colors.length / 2)) * 0.5;
+			})
+		);
 
 	$: path = `M${[...data]
 		.splice(30)
 		.map((d) => `${xScale(d.year)},${yScale(d.path)}`)
 		.join('L')}`;
 
+	$: for (let i = 0; i < 200; i++) {
+		console.log(`%c getting color ${i} ${colorScale(i)}`, `background-color: ${colorScale(i)}`);
+	}
 	$: getColor = function (datapoint) {
+		// console.log(
+		// 	`%c getting color ${datapoint.averageTemperature} ${colorScale(
+		// 		datapoint.averageTemperature
+		// 	)}`,
+		// 	`background-color: ${colorScale(datapoint.averageTemperature)}`
+		// );
 		return colorScale(datapoint.averageTemperature);
 	};
 
 	$: selectedDatapoint = null;
+
+	$: selectedStationName = 'station';
+
+	const wetterdienst = PUBLIC_VERSION == 'at' ? 'geosphere' : 'impact';
+	// $: Papa.parse(`../data/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
+	$: Papa.parse(`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
+		download: true,
+		dynamicTyping: true,
+		skipEmptyLines: true,
+		header: true,
+		complete: function (results) {
+			if (results) {
+				selectedStationName = results.data.find((d) => d.id == $selectedStation)?.name;
+			}
+		}
+	});
 </script>
 
 <div
@@ -91,24 +110,33 @@
 				on:mouseover={() => (selectedDatapoint = null)}
 			/>
 
-			<g transform={`translate(${margin.left}, ${margin.top})`}>
+			<g transform={`translate(${0}, ${margin.top})`}>
+				<line
+					x1={margin.left - 10}
+					x2={chartWidth - margin.right + 10}
+					y1={yScale(averageTemperature)}
+					y2={yScale(averageTemperature)}
+					stroke="black"
+					width="2"
+				/>
 				{#each xScale.ticks(chartWidth > 1000 ? 10 : 5).slice(0, -1) as tick}
-					<g transform={`translate(${xScale(tick)}, ${margin.top})`} class="text-xs text-gray-500">
+					<g
+						transform={`translate(${margin.left + xScale(tick)}, ${chartHeight - margin.bottom})`}
+						class="text-xs text-gray-500"
+					>
 						<text fill="currentColor">{tick}</text>
 					</g>
 				{/each}
 
 				{#each yScale.ticks(10).slice(0, -1) as tick}
 					<g transform={`translate(0, ${yScale(tick)})`} class="text-xs text-gray-500">
-						<text
-							fill="currentColor"
-							x={margin.left + innerChartWidth + 5}
-							dominant-baseline="middle">{tick} {unit}</text
+						<text fill="currentColor" x={chartWidth - margin.right + 15} dominant-baseline="middle"
+							>{tick.toString().replace('.', ',')} {unit}</text
 						>
 						<line
-							x1="0"
+							x1={margin.left - 10}
 							y1="0"
-							x2={margin.left + innerChartWidth}
+							x2={chartWidth - margin.right + 10}
 							y2="0"
 							stroke="currentColor"
 							stroke-width="2"
@@ -116,12 +144,31 @@
 						/>
 					</g>
 				{/each}
+
+				<!-- Info left -->
+				<text
+					x={margin.left - 30 - 5}
+					y={yScale(averageTemperature)}
+					dominant-baseline="start"
+					font-size="30"
+					font-weight="bold">∅</text
+				>
+				<text x={10} y={yScale(averageTemperature)}>
+					<tspan x="0" dy="{0}em" font-weight="bold">Durchschnitts-</tspan>
+					<tspan x="0" dy="{1.2}em" font-weight="bold">temperatur</tspan>
+					<tspan x="0" dy="{1.2}em" font-weight="bold"
+						>{averageTemperature.toFixed(2).replace('.', ',')} °C</tspan
+					>
+					<tspan x="0" dy="{1.2}em" font-size="0.8em"
+						>{selectedStationName} ({$selectedStation})</tspan
+					></text
+				>
 			</g>
 
 			<g
 				id="visualisation"
 				transform={`translate(${margin.left}, ${margin.top})`}
-				class="text-gray-500 "
+				class="text-gray-500"
 			>
 				{#if path}
 					<path d={path} transition:draw />
@@ -130,7 +177,10 @@
 				{#each data as datapoint, i}
 					{@const barOffset = yScale(datapoint.averageTemperature) - yScale(averageTemperature)}
 					{@const barHeight = Math.abs(barOffset)}
-					{@const barY = barOffset > 0 ? yScale(datapoint.averageTemperature) - barHeight : yScale(datapoint.averageTemperature)}
+					{@const barY =
+						barOffset > 0
+							? yScale(datapoint.averageTemperature) - barHeight
+							: yScale(datapoint.averageTemperature)}
 					<g
 						transform={`translate(${xScale(datapoint.year)}, ${barY})`}
 						in:fade={{ delay: i * 10 }}
@@ -156,16 +206,24 @@
 								/>
 							</circle>
 						{/if}
-						<rect width={radius} height={barHeight} fill={getColor(datapoint)} class="cursor-help"
-						on:mouseover={() => (selectedDatapoint = datapoint)}></rect>
+						<rect
+							width={radius}
+							height={barHeight}
+							fill={getColor(datapoint)}
+							class="cursor-help"
+							on:mouseover={() => (selectedDatapoint = datapoint)}
+						/>
 					</g>
 				{/each}
 
 				{#if selectedDatapoint}
+					{@const offset = selectedDatapoint.averageTemperature - averageTemperature}
 					<g
 						transform="translate({xScale(selectedDatapoint.year) > chartWidth - 300
 							? xScale(selectedDatapoint.year) - 280 + radius * 2
-							: xScale(selectedDatapoint.year) - radius},{yScale(selectedDatapoint.averageTemperature) + radius})"
+							: xScale(selectedDatapoint.year) - radius},{yScale(
+							selectedDatapoint.averageTemperature
+						) + radius})"
 						class="text-white text-sm uppercase cursor-help"
 						transition:fade
 					>
@@ -181,7 +239,9 @@
 							>{selectedDatapoint.year}</text
 						>
 						<text x="45" y="-1" fill="currentColor" dominant-baseline="middle"
-							>{selectedDatapoint.averageTemperature}{unit}</text
+							>{selectedDatapoint.averageTemperature}
+							{unit} ({offset > 0 ? '+' : '-'}{Math.abs(offset).toFixed(1).replace('.', ',')}
+							{unit})</text
 						>
 						<text x="90" y="-1" fill="currentColor" dominant-baseline="middle" class="text-xs"
 							>{yLabel}</text
@@ -192,3 +252,20 @@
 		</svg>
 	{/if}
 </div>
+
+<style>
+	.hsl-colors {
+		color: hsl(216, 86%, 23%);
+		color: hsl(208, 69%, 42%);
+		color: hsl(204, 54%, 52%);
+		color: hsl(202, 57%, 63%);
+		color: hsl(201, 53%, 75%);
+		color: hsl(209, 56%, 86%);
+		color: hsl(209, 61%, 92%);
+		color: hsl(19, 96%, 91%);
+		color: hsl(17, 94%, 81%);
+		color: hsl(14, 96%, 72%);
+		color: hsl(11, 96%, 64%);
+		color: hsl(5, 86%, 55%);
+	}
+</style>
--- src/lib/components/charts/temperatureAverageGradient/index.svelte
+++ src/lib/components/charts/temperatureAverageGradient/index.svelte
@@ -58,21 +58,6 @@
 		// const upperBoundIndex = historicalDataset.findIndex((d) => d.year == upperBoundYear);
 		return averageSum + entry.averageTemperature / historicalDataset.length;
 	}, 0);
-
-	$: selectedStationName = 'station';
-
-	// $: Papa.parse(`../data/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
-	$: Papa.parse(`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
-		download: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-		header: true,
-		complete: function (results) {
-			if (results) {
-				selectedStationName = results.data.find((d) => d.id == $selectedStation)?.name;
-			}
-		}
-	});
 </script>
 
 {#if historicalDataset}
@@ -82,22 +67,5 @@
 			averageTemperature={historicalAverageTemperature}
 			{unit}
 		/>
-
-		<div class="text-sm text-gray-600 absolute top-9 md:top-11 w-full pointer-events-none">
-			<div
-				class="container flex flex-col sm:flex-row sm:items-center sm:space-x-4 transform translate-y-2"
-			>
-				<div class="flex items-center space-x-1">
-					<div class="w-3 h-3 rounded-full" style="background-color: #AD0826" />
-					<p>Jahresdurchschnittstemperatur</p>
-				</div>
-				<div class="flex items-center space-x-1">
-					<div class="w-8 h-1 rounded-full bg-gray-200" />
-					<p>30-jähriger Durchschnitt</p>
-				</div>
-
-				<p>ID{$selectedStation} – {selectedStationName}</p>
-			</div>
-		</div>
 	</div>
 {/if}
