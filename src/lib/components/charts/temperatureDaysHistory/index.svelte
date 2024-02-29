4fc3e1f89f6fa261554ccf3c9d84c5b2c487c435
Markus Doppler <48652934+markusdoppler@users.noreply.github.com>
2024-02-29 07:28:52 +0100

    implemented basic temperture days history

--- src/lib/components/charts/temperatureDaysHistory/index.svelte
+++ src/lib/components/charts/temperatureDaysHistory/index.svelte
@@ -5,23 +5,31 @@
 	import { types } from '$lib/stores/weather';
 	import Loader from '$lib/components/Loader.svelte';
 	import { PUBLIC_VERSION } from '$env/static/public';
-
-	$: if ($page.url.searchParams.get('weatherStation')) {
-		$selectedStation = $page.url.searchParams.get('weatherStation');
-	} else {
-		$selectedStation = 427;
-	}
+	import BarChart from '../chartBar.svelte';
 
 	const wetterdienst = PUBLIC_VERSION == 'at' ? 'geosphere' : 'impact';
+
+	$: selectedStationData = [];
+	$: selectedStationName = 'station';
 
 	$: earliestPossibleYear = 1961;
 	$: latestPossibleYear = 2022;
 	$: selectedReferenceYears = 1961;
 
-	$: selectedStationData = [];
+	$: Papa.parse(`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
+		download: true,
+		dynamicTyping: true,
+		skipEmptyLines: true,
+		header: true,
+		complete: function (results) {
+			if (results) {
+				selectedStationName = results.data.find((d) => d.id == $selectedStation).name;
+			}
+		}
+	});
 
 	$: Papa.parse(
-		`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations/${selectedStation}/yearly-today.csv`,
+		`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations/${$selectedStation}/yearly.csv`,
 		{
 			download: true,
 			dynamicTyping: true,
@@ -37,21 +45,17 @@
 		}
 	);
 
-	$: selectedStationName = 'station';
-
-	$: Papa.parse(`https://data.klimadashboard.org/${PUBLIC_VERSION}/${wetterdienst}/stations.csv`, {
-		download: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-		header: true,
-		complete: function (results) {
-			if (results) {
-				selectedStationName = results.data.find((d) => d.id == selectedStation).name;
-			}
-		}
-	});
+	$: console.log(selectedStationData);
 </script>
 
 <p>Temperaturtageverlauf für {selectedStationName}:</p>
 
-
+{#if selectedStationData}
+	{#each $types as type}
+		<p>{@html type.icon} {type.label}</p>
+		<p><small>{type.description}</small></p>
+		<BarChart data={selectedStationData.map(d => { return { label: d.year, value: d[type.key+"s"] }; })} color={type.color} xAxixInterval={10} unit={''} />
+	{/each}
+{:else}
+	<Loader />
+{/if}
