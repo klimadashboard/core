a0c5f59ba9d08fbffd8dc72716b334b8264a1b1d
lisagsb <89943216+lisagsb@users.noreply.github.com>
2024-07-06 12:51:21 +0200

    include all years in the right order

--- src/lib/components/charts/emissionByFederalStateDE/index.svelte
+++ src/lib/components/charts/emissionByFederalStateDE/index.svelte
@@ -16,7 +16,6 @@
 		complete: function (results) {
 			if (results) {
 				rawData = results.data;
-				console.log(rawData);
 				regions = [
 					...new Set(
 						rawData.map((d) => {
@@ -35,29 +34,53 @@
 		}
 	});
 
-	$: selectedRegion = 'Bayern';
+	$: selectedRegion = 'Deutschland';
 	$: selectedGas = 'GHG';
 
 	$: dataset = [];
 
-	$: if (rawData) {
-		dataset = rawData
-			.filter((d) => d.region == selectedRegion && d.gastype == selectedGas && d.sector == 'total')
-			.map((e) => {
-				return {
-					label: e.year,
-					value: e.value
-				};
-			});
+	// Create complete dataset
+	function createCompleteDataset(rawData, selectedRegion, selectedGas) {
+		let filteredData = rawData.filter((d) => d.region === selectedRegion && d.gastype === selectedGas && d.sector === 'total');
+		
+		if (filteredData.length === 0) {
+			return [];
+		}
+		// Determine year range
+		let years = filteredData.map(d => d.year);
+		let minYear = Math.min(...years);
+		let maxYear = Math.max(...years);
+
+		// Generating all years within the range
+		let allYears = [];
+		for (let year = minYear; year <= maxYear; year++) {
+			allYears.push(year);
+		}
+		let data = filteredData.reduce((acc, e) => {
+			acc[e.year] = e.value;
+			return acc;
+		}, {});
+
+		// Mapping all years to the dataset
+		return allYears.map((year) => ({
+			label: year.toString(),
+			value: data[year] !== undefined ? data[year] : 'NA' 
+		}));
 	}
 
-	$: console.log(dataset);
+	// Updating dataset
+	$: if (rawData) {
+		dataset = createCompleteDataset(rawData, selectedRegion, selectedGas);
+	}
+
 </script>
 
 {#if dataset.length > 0}
 	<Filter {regions} bind:selectedRegion {gastypes} bind:selectedGas />
 	<div class="h-80">
-		<BarChart data={dataset} unit={'t ' + selectedGas} />
+		<BarChart 
+		data={dataset} 
+		unit={'t ' + selectedGas} />
 	</div>
 {:else}
 	<Loader />
