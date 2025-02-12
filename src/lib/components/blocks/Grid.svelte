e10a3df48ac80369b772cec8b28a69c19ed014d8
David Jablonski <web@davidjablonski.at>
2025-02-12 10:59:47 +0100

    add support for grid mobile adjustments

--- src/lib/components/blocks/Grid.svelte
+++ src/lib/components/blocks/Grid.svelte
@@ -1,28 +1,14 @@
 <script>
 	import Block from './index.svelte';
 	export let block;
-
-	const map = [{
-		width: 1,
-		classes: "col-span-1"
-	},{
-		width: 2,
-		classes: "col-span-2"
-	}, {
-		width: 3,
-		classes: "col-span-2 lg:col-span-3"
-	}, {
-		width: 4,
-		classes: "col-span-2 lg:col-span-4"
-	}]
 </script>
 
 <div class="grid gap-1 grid-cols-2 lg:grid-cols-4 p-1">
 	{#each block.blocks as b}
-		<div class="{map.find(d => d.width == b.width)?.classes} rounded-2xl overflow-hidden row-span-{b.height}">
+		<div class="{b.width} rounded-2xl overflow-hidden row-span-{b.height}">
 			<Block data={[b]} />
 		</div>
 	{/each}
 </div>
 
-<!-- row-span-1 row-span-2 row-span-3 row-span-4-->
+<!-- row-span-1 row-span-2 row-span-3 row-span-4 col-span-1 col-span-2 col-span-2 lg:col-span-3 lg:col-span-1 lg:col-span-4-->
