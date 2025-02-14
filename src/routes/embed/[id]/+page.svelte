401490a677503181b48c1e241e8b2a2565556cfa
David Jablonski <web@davidjablonski.at>
2025-02-14 16:44:18 +0100

    make sure old embeds still work

--- src/routes/embed/[id]/+page.svelte
+++ src/routes/embed/[id]/+page.svelte
@@ -2,13 +2,10 @@
 	import { page } from '$app/stores';
 	import Chart from '$lib/components/charts/index.svelte';
 
-	$: id = $page.params.slug.replace('charts/', '');
 	const bgColor = $page.url.searchParams.get('bgcolor');
 	const showText = $page.url.searchParams.get('showtext');
 </script>
 
 <main style="background: #{bgColor}" class="w-screen h-screen">
-	{#if $page.params.slug}
-		<Chart {id} showText={showText == 'false' ? false : true} />
-	{/if}
+	<Chart id={$page.data.id} showText={showText == 'false' ? false : true} />
 </main>
