d1e6fc6c5fd8a9d4391e31d54513507d61628068
David Jablonski <web@davidjablonski.at>
2025-02-27 09:01:43 +0100

    feat(policies): date display for updates

--- src/routes/(main)/[[lang=lang]]/policies/PolicyUpdates.svelte
+++ src/routes/(main)/[[lang=lang]]/policies/PolicyUpdates.svelte
@@ -19,7 +19,7 @@
 	{#each updates.sort((a, b) => dayjs(b.date).diff(dayjs(a.date))) as update}
 		<SplideSlide>
 			<div class="border-t-2 w-[80vh] max-w-sm pt-2">
-				<p class="text-sm opacity-80 font-bold">{dayjs(update.date).format('DD.MM.YYYY')}</p>
+				<p class="text-sm opacity-80 font-bold">{dayjs(update.date).format('MMMM YYYY')}</p>
 				<h2 class="text-2xl">{update.title}</h2>
 				<div class="text-lg text">{@html update.text}</div>
 			</div>
