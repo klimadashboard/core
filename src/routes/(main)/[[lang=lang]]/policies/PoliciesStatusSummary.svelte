b325ed91f4ab31441e39c3f9c23ddf62b0037ff5
David Jablonski <web@davidjablonski.at>
2025-03-02 22:27:32 +0100

    feat(policies): description for stati

--- src/routes/(main)/[[lang=lang]]/policies/PoliciesStatusSummary.svelte
+++ src/routes/(main)/[[lang=lang]]/policies/PoliciesStatusSummary.svelte
@@ -12,18 +12,31 @@
 				? page.data.translations.onePolicyFound
 				: policies.length + ' ' + page.data.translations.policiesFound}
 	</p>
-	<p>
+	<ul class="flex gap-4">
 		{#each page.data.status.filter((d) => policies.filter((p) => p.status.key == d.key).length > 0) as status}
-			<span class="mr-3">
+			<li class="flex items-center gap-1">
 				<div
-					class="w-2 h-2 rounded-full inline-block -translate-y-0.5"
-					style="background: {status.color}"
-				></div>
-				{policies.filter((p) => p.status.key == status.key).length}
-				{status.label}
-			</span>
+					class="w-6 h-6 rounded-full font-bold grid"
+					style="background: {status.color}; color: {status.colorText}"
+				>
+					<span class="m-auto">
+						{policies.filter((p) => p.status.key == status.key).length}
+					</span>
+				</div>
+				{#if status.description}
+					<div class="underline underline-offset-2 decoration-current/20 relative group">
+						{status.label}
+						<span
+							class="hidden group-hover:block absolute bottom-full left-1/2 bg-gray-100 dark:bg-gray-900 p-3 text-sm text-center w-64 -translate-x-1/2"
+							>{status.description}</span
+						>
+					</div>
+				{:else}
+					{status.label}
+				{/if}
+			</li>
 		{/each}
-	</p>
+	</ul>
 	<div class="rounded-full h-2 mt-1 w-full flex overflow-hidden">
 		{#each page.data.status as status, i}
 			<div
