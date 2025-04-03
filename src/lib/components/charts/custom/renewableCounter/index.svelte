2873b5ca6a382346463f19d9213cbd4621d2ae44
christina <ginihumer96@yahoo.de>
2025-04-03 14:38:32 +0200

    update renewable counter data fetching

--- src/lib/components/charts/custom/renewableCounter/index.svelte
+++ src/lib/components/charts/custom/renewableCounter/index.svelte
@@ -1,8 +1,33 @@
 <script>
 	import Chart from './Chart.svelte';
 	import Papa from 'papaparse';
+	import getDirectusInstance from '$lib/utils/directus';
+	import { readItems } from '@directus/sdk';
+	import { PUBLIC_VERSION } from '$env/static/public';
 
 	let dataGoals;
+	const getGoals = async function () {
+		try{
+			const directus = getDirectusInstance(fetch);
+			const goals = await directus.request(
+				readItems('erneuerbare_2030_scenarios', {
+					filter: {
+						_and: [
+							{ 
+								Country: { _eq: PUBLIC_VERSION.toUpperCase() },
+							}
+						]
+					},
+					limit: -1
+				})
+			);
+			dataGoals = goals;
+			
+		} catch (error) {
+			console.error('Error fetching suggestions:', error);
+		}
+	};
+	$: getGoals();
 
 	Papa.parse(
 		'https://data.klimadashboard.org/at/energy/renewables/erneuerbare_2030_scenarios.csv',
@@ -19,31 +44,43 @@
 		}
 	);
 
-	let dataPV;
-	Papa.parse('https://data.klimadashboard.org/at/energy/renewables/pv_produktion.csv', {
-		download: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-		header: true,
-		complete: function (results) {
-			if (results) {
-				dataPV = results.data;
-			}
+	const getProduction = async function (dataKey) {
+		try{
+			const directus = getDirectusInstance(fetch);
+			const production = await directus.request(
+				readItems(dataKey + '_produktion', {
+					filter: {
+						_and: [
+							{ 
+								Country: { _eq: PUBLIC_VERSION.toUpperCase() },
+							}
+						]
+					},
+					limit: -1
+				})
+			);
+			return production
+			
+		} catch (error) {
+			console.error('Error fetching suggestions:', error);
 		}
-	});
+	};
+
+	let dataPV;
+	const getPVProduction = async function(){
+		dataPV = await getProduction("pv")
+		console.log("directus pv", dataPV)
+	}
+	$: getPVProduction();
+
 
 	let dataWind;
-	Papa.parse('https://data.klimadashboard.org/at/energy/renewables/windkraft_produktion.csv', {
-		download: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-		header: true,
-		complete: function (results) {
-			if (results) {
-				dataWind = results.data;
-			}
-		}
-	});
+	const getWindProduction = async function(){
+		dataWind = await getProduction("windkraft")
+		console.log("directus wind", dataWind)
+	}
+	$: getWindProduction();
+
 </script>
 
 {#if dataPV && dataWind && dataGoals}
