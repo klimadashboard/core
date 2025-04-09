e52da985a56c86e1c443463b271fb9ab4b5a8331
David Jablonski <web@davidjablonski.at>
2025-04-09 15:39:50 +0200

    feat(regions): add beta disclaimer

--- src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
+++ src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
@@ -145,6 +145,27 @@
 			<div class="h-80">
 				<Map {data} />
 			</div>
+			<div class="container p-4 mt-4 opacity-70">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="inline -translate-y-1"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+						d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"
+					/><path
+						d="M15.03 17.478a8.797 8.797 0 0 1 -3.03 .522c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6a20.48 20.48 0 0 1 -.258 .419"
+					/><path d="M19 16v3" /><path d="M19 22v.01" /></svg
+				>
+				You are accessing a <b>beta version</b> of regional climate dashboards. Note that some data and
+				visualisations might be incomplete or show incorrect readings.
+			</div>
 			<Intro {data} />
 			<div class="sticky top-12 py-4 bg-white dark:bg-dark-950 z-50" id="top">
 				<div class="container overflow-scroll">
