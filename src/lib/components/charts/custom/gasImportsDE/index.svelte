02e720be3fabfb3586979b4a28ab75c0cfd20ece
christina <ginihumer96@yahoo.de>
2025-04-10 10:28:30 +0200

    start adapting gas_imports chart to directus api

--- src/lib/components/charts/custom/gasImportsDE/index.svelte
+++ src/lib/components/charts/custom/gasImportsDE/index.svelte
@@ -2,20 +2,9 @@
 	import ChartLine from '$lib/components/charts/chartLine.svelte';
 	import Papa from 'papaparse';
 	import dayjs from 'dayjs';
-
-	let rawData;
-
-	Papa.parse('https://data.klimadashboard.org/de/energy/fossil/gas_imports-bundesnetzagentur.csv', {
-		download: true,
-		dynamicTyping: true,
-		header: true,
-		skipEmptyLines: true,
-		complete: function (results) {
-			if (results) {
-				rawData = results.data;
-			}
-		}
-	});
+	import { PUBLIC_VERSION } from '$env/static/public';
+	import getDirectusInstance from '$lib/utils/directus';
+	import { readItems } from '@directus/sdk';
 
 	const keys = [
 		'Tschechien',
@@ -61,6 +50,64 @@
 		'#072F58'
 	];
 
+	let dataGasImports;
+	const getGasImportDataBySource = async function (source_name) {
+		try {
+			const directus = getDirectusInstance(fetch);
+			let gas_imports = await directus.request(
+				readItems('gas_imports', {
+					filter: {
+						_and: [
+							{
+								Country: { _eq: PUBLIC_VERSION.toUpperCase() }
+							},
+							{
+								_or: [
+									{
+										import_country: {
+											name_de: { _eq: source_name }
+										}
+									},
+									{ import_source: { _eq: source_name } }
+								]
+							}
+						]
+					},
+					limit: -1,
+					fields: ['import_country.name_de', 'import_source', 'date', 'value'],
+					sort: ['date']
+				})
+			);
+			gas_imports = gas_imports.map((row) => ({
+				...row,
+				date: new Date(row.date).toLocaleDateString('de-DE', {
+					day: '2-digit',
+					month: '2-digit',
+					year: 'numeric'
+				})
+			}));
+			console.log(source_name, ': ', gas_imports);
+		} catch (error) {
+			console.error('Error fetching suggestions:', error);
+		}
+	};
+
+	$: getGasImportDataBySource('Deutschland Import');
+
+	let rawData;
+
+	Papa.parse('https://data.klimadashboard.org/de/energy/fossil/gas_imports-bundesnetzagentur.csv', {
+		download: true,
+		dynamicTyping: true,
+		header: true,
+		skipEmptyLines: true,
+		complete: function (results) {
+			if (results) {
+				rawData = results.data;
+			}
+		}
+	});
+
 	$: data = rawData?.splice(rawData.length - 365).map((e, i) => {
 		return {
 			...e,
@@ -68,6 +115,7 @@
 			label: e['                     .']
 		};
 	});
+	$: console.log(data);
 </script>
 
 <div class="h-80">
