eb2548c3e5edbf3dcaee504889096619d0fdaba1
lisagsb <89943216+lisagsb@users.noreply.github.com>
2025-04-15 16:45:42 +0200

    feat(renewableRegions): start headings and text

--- src/lib/components/charts/custom/renewableRegions/index.svelte
+++ src/lib/components/charts/custom/renewableRegions/index.svelte
@@ -102,6 +102,7 @@
 				colors={colors.find((c) => c.key === selectedEnergy).colors}
 				{regions}
 				{data}
+				{selectedEnergy}
 				region={getRegionData(regions, selectedRegion, countryName)}
 				bind:selectedRegion
 			/>
