add4c85c5bd8b5792744a5b2520fc140a7660e7a
David Jablonski <web@davidjablonski.at>
2025-04-24 15:23:14 +0200

    fix(renewableShare): ts lang declaration

--- src/lib/components/charts/custom/mobilityRenewableShare/index.svelte
+++ src/lib/components/charts/custom/mobilityRenewableShare/index.svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<script context="module" lang="ts">
 	// Directive to handle clicks outside of an element
 	function clickOutside(node: HTMLElement, handler: (event: MouseEvent) => void) {
 		const handleClick = (event: MouseEvent) => {
