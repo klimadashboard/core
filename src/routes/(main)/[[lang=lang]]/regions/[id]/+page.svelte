44e612b5a1fe51bd56f07f70cb76af5d7e88789f
David Jablonski <web@davidjablonski.at>
2025-05-21 11:28:03 +0200

    feat(regions): add icon for heating

--- src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
+++ src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
@@ -76,7 +76,8 @@
 					id: '8267b6b9-605d-4603-a8b4-4ad9e6a3c553',
 					countries: ['de']
 				}
-			]
+			],
+			icon: "<svg  xmlns='http://www.w3.org/2000/svg'  width='24'  height='24'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-brand-walmart'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M12 8.04v-5.04' /><path d='M15.5 10l4.5 -2.5' /><path d='M15.5 14l4.5 2.5' /><path d='M12 15.96v5.04' /><path d='M8.5 14l-4.5 2.5' /><path d='M8.5 10l-4.5 -2.505' /></svg>"
 		},
 		{
 			title: 'Mobilität',
