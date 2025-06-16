cc63928b317f77e9f320b9384ed5a6d1c20c28e5
David Jablonski <web@davidjablonski.at>
2025-06-16 17:51:24 +0200

    feat(support): add donation

--- src/routes/(main)/[[lang=lang]]/regions/[id]/Support.svelte
+++ src/routes/(main)/[[lang=lang]]/regions/[id]/Support.svelte
@@ -12,16 +12,35 @@
 			class="h-24 mt-4"
 		/>
 	</div>
-	<div class="max-w-2xl">
+	<div class="max-w-2xl text-lg">
 		<h2 class="text-2xl font-bold">Werde regionaler Klimadashboard-Supporter!</h2>
-		<p>
+		<p class=" leading-snug">
 			Spende jetzt fürs Klimadashboard und hilf uns, weitere Visualisierungen für deine Region zu
-			entwickeln. Als Dank zeigen wir dich als Supporter auf deinem regionalen Klimadashboard an.
+			entwickeln. Als Dank zeigen wir dich ab Juli 2025 als Supporter auf deinem regionalen
+			Klimadashboard an.
 		</p>
 		<a
 			href="https://donate.stripe.com/8wM03o9ZS0OX28U4gg"
 			target="_blank"
-			class="button mt-2 inline-block">Jetzt spenden</a
+			class="button mt-2 w-max flex items-center gap-1"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="icon icon-tabler icons-tabler-outline icon-tabler-credit-card-pay"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+					d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5"
+				/><path d="M3 10h18" /><path d="M16 19h6" /><path d="M19 16l3 3l-3 3" /><path
+					d="M7.005 15h.005"
+				/><path d="M11 15h2" /></svg
+			><span>Jetzt spenden</span></a
 		>
 	</div>
 </div>
