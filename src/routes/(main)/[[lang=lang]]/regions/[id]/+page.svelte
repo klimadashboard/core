8b6d769156b56f4cdcbbd34eb6fb7c2980132664
David Jablonski <web@davidjablonski.at>
2026-01-20 15:45:51 +0100

    fix(regions): broken charts ahead of update

--- src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
+++ src/routes/(main)/[[lang=lang]]/regions/[id]/+page.svelte
@@ -47,7 +47,7 @@
 			icon: "<svg  xmlns='http://www.w3.org/2000/svg'  width='24'  height='24'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-cloud'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M6.657 18c-2.572 0 -4.657 -2.007 -4.657 -4.483c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 1.927 -1.551 3.487 -3.465 3.487h-11.878' /></svg>",
 			charts: [
 				{
-					id: 'cae6032b-86a9-45d0-bc11-17343845b25a',
+					id: '9be927d7-4918-40fe-9fb8-9873dd78bcc7',
 					countries: ['at', 'de']
 				}
 			]
@@ -73,7 +73,7 @@
 			icon: "<svg  xmlns='http://www.w3.org/2000/svg'  width='24'  height='24'  viewBox='0 0 24 24'  fill='none'  stroke='currentColor'  stroke-width='2'  stroke-linecap='round'  stroke-linejoin='round'  class='icon icon-tabler icons-tabler-outline icon-tabler-steering-wheel'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' /><path d='M12 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' /><path d='M12 14l0 7' /><path d='M10 12l-6.75 -2' /><path d='M14 12l6.75 -2' /></svg>",
 			charts: [
 				{
-					id: '68b0f853-b1b1-4120-aedd-87de58ea3209',
+					id: '600f66fa-0b6c-4425-a7c6-1281f00d9ff1',
 					countries: ['at', 'de']
 				},
 				{
